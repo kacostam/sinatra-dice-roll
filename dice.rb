476124cc39bc734dd59202d1b7c0d8f294f38fcb
Karla Acosta <77418536+kacostam@users.noreply.github.com>
2025-01-29 05:55:03 +0000

    included better_errors

--- dice.rb
+++ dice.rb
@@ -2,6 +2,13 @@
 
 require "sinatra"
 require "sinatra/reloader"
+require "better_errors"
+require "binding_of_caller"
+
+# better error visualization -- need this configuration for better_errors
+use(BetterErrors::Middleware)
+BetterErrors.application_root = __dir__
+BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 # create root path
 get ('/') do
