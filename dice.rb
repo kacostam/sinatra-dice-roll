727fc38b281667ccf79091cf765f14d1a4f9cf37
Karla Acosta <77418536+kacostam@users.noreply.github.com>
2025-01-29 05:48:52 +0000

    created hyperlinks on index page

--- dice.rb
+++ dice.rb
@@ -3,8 +3,16 @@
 require "sinatra"
 require "sinatra/reloader"
 
+# create root path
 get ('/') do
-  "Hello World"
+  "<h1>Dice Roll</h1>
+   <ul>
+    <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
+    <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
+    <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
+    <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
+    <li><a href=\"/dice/3/7\">Roll three 7-sided dice</a></li>
+  </ul>"
 end
 
 get("/zebra") do
