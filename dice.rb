5ae173f82979bd9a7cb995e4d649e36780834601
Karla Acosta <77418536+kacostam@users.noreply.github.com>
2025-01-30 01:13:25 +0000

    created erb file for every dice roll

--- dice.rb
+++ dice.rb
@@ -29,13 +29,12 @@ end
 # 2 6-sided dice
 get("/dice/2/6") do
   first_die = rand(1..6)
-  second_die = rand(1..6) 
+  second_die = rand(1..6)
   sum = first_die + second_die
 
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 
-  "<h1>2d6</h1>
-  <p>#{outcome}</p>"
+  erb(:two_six)
 end
 
 
@@ -44,19 +43,18 @@ get("/dice/2/10") do
   first_die = rand(1..10)
   second_die = rand(1..10)
   sum = first_die + second_die
-  outcome = "You got a #{first_die} and a #{second_die} for a total of #{sum}."
-"<h1>2d10</h1>
-<p>#{outcome}</p>"
+  @outcome = "You got a #{first_die} and a #{second_die} for a total of #{sum}."
+
+  erb(:two_ten)
 
 end
 
 # 1 20-sided die
 get("/dice/1/20") do
   die = rand(1..20)
-  outcome = "You got a #{die}."
-"<h1>1d20</h1>
-<p>#{outcome}</p>"
+  @outcome = "You got a #{die}."
 
+    erb(:one_twenty)
 end
 
 # 5 4-sided dice
@@ -67,10 +65,9 @@ get("/dice/5/4") do
   fourth_die = rand(1..4)
   fifth_die = rand(1..4)
   sum = first_die + second_die + third_die + fourth_die + fifth_die
-  outcome = "You got a #{first_die}, #{second_die}, #{third_die}, #{fourth_die} and a #{fifth_die}, for a total of #{sum}."
+  @outcome = "You got a #{first_die}, #{second_die}, #{third_die}, #{fourth_die} and a #{fifth_die}, for a total of #{sum}."
 
-  "<h1>5d4</h1>
-  <p>#{outcome}</p>"
+  erb(:five_four)
 end
 
 # 3 7-sided dice
@@ -79,8 +76,7 @@ get("/dice/3/7") do
   second_die = rand(1..7) 
   third_die = rand(1..7)
   sum = first_die + second_die + third_die
-  outcome = "You got a #{first_die}, #{second_die} and a #{third_die} for a total of #{sum}."
-  "<h1>3d7</h1>
-  <p>#{outcome}</p>"
+  @outcome = "You got a #{first_die}, #{second_die} and a #{third_die} for a total of #{sum}."
 
+  erb(:three_seven)
 end
